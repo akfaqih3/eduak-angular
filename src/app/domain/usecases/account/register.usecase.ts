b069c3135f54151d3d611f7ad66eb396ca690514
akfaqih3 <akfaqih3@gmail.com>
2025-11-27 14:59:47 -0800

    register use case

---
+++ src/app/domain/usecases/account/register.usecase.ts
@@ -0,0 +1,14 @@
+import { Injectable, inject } from '@angular/core';
+import { AccountRepository } from '../../repositories/account.repository';
+import { AccountEntity } from '../../entities/account.entity';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RegisterUseCase {
+  private repository = inject(AccountRepository);
+
+  execute(account: AccountEntity): Promise<AccountEntity> {
+    return this.repository.register(account);
+  }
+}
--- src/app/domain/usecases/index.ts
+++ src/app/domain/usecases/index.ts
@@ -1,6 +1,7 @@
 // Account UseCases
 export * from './account/get-profile.usecase';
 export * from './account/update-profile.usecase';
+export * from './account/register.usecase';
 
 // Course UseCases
 export * from './course/get-courses.usecase';
